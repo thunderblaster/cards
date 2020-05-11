140e504190c5fbdcd06c14e781b015fb75c6adca
Nick Heidke <nickh@mac12139.local>
2020-05-11 11:37:27 -0500

    Allowing vs code to debug docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 cd /usr/src/cards && npm update;
 
 # START NODEMON / EXPRESS
-(cd /usr/src/cards && nodemon /usr/src/cards/server.js localhost 3001 &) >> /log/app.log;
+(cd /usr/src/cards && nodemon /usr/src/cards/server.js localhost 3001 --debug=5858 &) >> /log/app.log;
 
 # START SASS COMPILING
 (compass watch /usr/src/cards &) >> /log/app.log;
