bff5382801ef1022d48b0613628290e55145afdf
Nick Heidke <nickh@mac12139.local>
2020-05-12 11:23:54 -0500

    Adding approved column to card tables

--- db/tables/white_cards.sql
+++ db/tables/white_cards.sql
@@ -1,4 +1,6 @@
 CREATE TABLE `white_cards` (
-  `card_id` int(11) DEFAULT NULL,
-  `card_text` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `card_id` int(11) NOT NULL AUTO_INCREMENT,
+  `card_text` text,
+  `approved` bit(1) DEFAULT b'1',
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4824 DEFAULT CHARSET=latin1;
